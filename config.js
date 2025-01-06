4b36ba643788119842b3596205cb92eb6b5d35f6
Funny Dev <105073457+manhba-dev@users.noreply.github.com>
2025-01-06 19:25:11 +0700

    update file config

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    introTitle: 'Hê nhô!!',
+    introDesc: `Đây là web tỏ tình bản demo thôi, bao giờ gửi cho crush thì sửa đoạn này sau =)) 
+    Chứ mấy ông đi đồn thật thì chết tôi T.T`,
+    btnIntro: 'Vô coi nè',
+    title: 'Từ lần đầu gặp em anh đã thích em rồi. Nhất là khi em cười, em thật sự rất đẹp 😙',
+    desc: 'Vì vậy anh rất muốn em là người yêu của anh crush ạ. Em cũng thích anh chứ!!',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại chỉ thích em nhỉ <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Cảm ơn em đã cho anh cơ hội.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
