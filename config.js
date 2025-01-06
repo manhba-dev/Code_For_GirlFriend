c71a457cb625ee9c6db08fc985dc905a07aef3d9
Funny Dev <105073457+manhba-dev@users.noreply.github.com>
2025-01-06 19:32:44 +0700

    update new file config

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
     introTitle: 'Hê nhô!!',
-    introDesc: `Đây là web tỏ tình bản demo thôi, bao giờ gửi cho crush thì sửa đoạn này sau =)) 
-    Chứ mấy ông đi đồn thật thì chết tôi T.T`,
+    introDesc: `Đây là web tỏ tình bản demo cho vui hoi, bao giờ gửi cho crush thì làm thật sau, bản này hơi sến =)) 
+    Chứ mấy bố đi đồn thật thì chết tôi T.T ( Anh lạ gì em đâu Hùng ^.^ )`,
     btnIntro: 'Vô coi nè',
     title: 'Từ lần đầu gặp em anh đã thích em rồi. Nhất là khi em cười, em thật sự rất đẹp 😙',
     desc: 'Vì vậy anh rất muốn em là người yêu của anh crush ạ. Em cũng thích anh chứ!!',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại chỉ thích em nhỉ <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại chỉ thích em nhỉ. Em có đoán được lý do không <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
